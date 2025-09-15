8320ca33f0fdd3b6ce86616cb8cd2344e300a744
User-19fff <lolkd005@gmail.com>
2025-09-15 22:48:28 +0200

    Kész

--- Program.cs
+++ Program.cs
@@ -2,37 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CiklusokCucc {
     internal class Program {
         static void Main(string[] args) {
-            // elso();
             // masodik();
             // harmadik();
             // negyedik();
             // otodik();
             // hatodik();
+            // hetedik();
+            // nyolcadik();
         }
 
-
+        // BM KÉSZ!
         static void elso() {
-            int n = Convert.ToInt32(Console.ReadLine());
+            var sb = new StringBuilder();
+            int n = 100;
 
-            IEnumerable<int> numbers = Enumerable.Range(1, n);
-            Console.WriteLine(string.Join(", ", numbers));
-            Console.WriteLine(string.Join(", ", numbers.Reverse()));
+            for (int i = 1; i <= n; i++) {
+                if (i > 1) sb.Append(", ");
+                sb.Append(i);
+            }
+
+            for (int i = n; i >= 1; i--) {
+                sb.Append(", ");
+                sb.Append(i);
+            }
+
+            Console.WriteLine(sb.ToString());
         }
 
+        // BM KÉSZ!
         static void masodik() {
             Random r = new Random();
-            int d = Convert.ToInt32(Console.ReadLine());
-            IEnumerable<int> dice = Enumerable.Range(1, d).Select(_ => r.Next(1, 7)).ToList();
+            int d = 100;
+            var dice = new List<int>(d);
+
+            for (int i = 0; i < d; i++) {
+                dice.Add(r.Next(1, 7));
+            }
 
             Console.WriteLine(string.Join(", ", dice));
             Console.WriteLine(dice.Count(i => i == 6));
         }
 
+        // BM KÉSZ!
         static void harmadik() {
             List<char> mh = new List<char>{ 'a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ö', 'ő', 'ú', 'ü', 'ű' };
             string a = Console.ReadLine();
@@ -46,15 +61,23 @@ namespace CiklusokCucc {
             Console.WriteLine($"{((double) eCount / mhCount) * 100}%");
         }
 
+        // BM KÉSZ!
         static void negyedik() {
             int n = Convert.ToInt32(Console.ReadLine());
-            double limit = Math.Ceiling(Math.Sqrt(n));
+            if (n < 2) Console.WriteLine("Osszetett");
+            if (n == 2) Console.WriteLine("Prim");
+            if (n % 2 == 0) Console.WriteLine("Osszetett");
 
-            bool isPrim = !Enumerable.Range(2, (int) limit - 1).Any(i => n % i == 0);
+            int limit = (int)Math.Sqrt(n);
 
-            Console.WriteLine(isPrim ? "Prim" : "Osszetett");
+            for (int i = 3; i <= limit; i += 2) {
+                if (n % i == 0) Console.WriteLine("Osszetett");
+            }
+
+            Console.WriteLine("Prim");
         }
 
+        // BM KÉSZ!
         static void otodik() {
             List<long> n = new List<long>();
             long a;
@@ -69,17 +92,226 @@ namespace CiklusokCucc {
             Console.WriteLine(atlag);
         }
 
+        // BM KÉSZ!
         static void hatodik() {
             int a = Convert.ToInt32(Console.ReadLine());
             int f = 1; // 0-val való szorzás nem lehet ugyebár
 
-            Enumerable.Range(1, a).ToList().ForEach(i => f *= i);
+            for (int i = 1; i <= a; i++) {
+                f *= i;
+            }
 
             Console.WriteLine(f);
         }
 
+        // Nem kell BM
         static void hetedik() {
+            int f;
 
+            do {
+                f = Convert.ToInt32(Console.ReadLine());
+            } while (f <= 1940 || f >= 2020);
+
+            Console.WriteLine(2020 - f);
+        }
+
+        static void nyolcadik() {
+            string m = "1 - Add meg a nevet\n2 - Üdv {0}!\n3 - Helló {0}!\n4 - Szia {0}!\n5 - Kilépés.";
+            string n = "";
+            bool running = true;
+
+            while (running) {
+                Console.WriteLine(m);
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                switch (a) {
+                    case 1:
+                        n = Console.ReadLine();
+                        break;
+                    case 2:
+                        if (string.IsNullOrEmpty(n)) {
+                            Console.WriteLine(m);
+                            break;
+                        }
+
+                        Console.WriteLine($"Üdv {n}");
+                        break;
+                    case 3:
+                        if (string.IsNullOrEmpty(n)) {
+                            Console.WriteLine(m);
+                            break;
+                        }
+
+                        Console.WriteLine($"Hello {n}");
+                        break;
+                    case 4:
+                        if (string.IsNullOrEmpty(n)) {
+                            Console.WriteLine(m);
+                            break;
+                        }
+
+                        Console.WriteLine($"Szia {n}");
+                        break;
+                    case 5:
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine(m);
+                        break;
+                }
+            }
+        }
+
+        static void kilencedik() {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 1; i * i <= n; i++) { // nem kell minden számot megnézni csak a gyök(n)-ig mivel ha i osztja n-t akkor n / i is osztó.
+                if (n % i == 0) {
+                    if (i * i == n) count += 1;
+                    else count += 2;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        static long lcm(int x, int y, int gcd) {
+            return ((long) x * y) / gcd;
+        }
+
+        static int gcd(int a, int b) {
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+
+        static void tizedik() {
+            int a = gcd(6, 10);
+
+            Console.WriteLine($"{a} {lcm(6, 10, a)}");
+        }
+
+        static void tizenegyedik() {
+            int x = 100;
+
+            if (x == 1) {
+                Console.WriteLine(1);
+                return;
+            }
+
+            List<string> factors = new List<string>();
+            int original = x;
+
+            while (x % 2 == 0) { // csak gyökig iterál, ha a szám összetett biztos van EGY primosztoja ami legfeljebb a gyökével egyenlő
+                factors.Add("2");
+                x /= 2;
+            }
+
+            for (int i = 3; i <= x * x; i += 2) { // 2-esével lép mivel a párosokat már kiszűrtük!
+                while (x % i == 0) {
+                    factors.Add(i.ToString());
+                    x /= i;
+                }
+            }
+
+            if (x > 1) { // ha a maradék nagyobb mint 1 az maga is prim!
+                factors.Add(x.ToString());
+            }
+
+            Console.WriteLine(string.Join("*", factors));
+        }
+
+        static void tizenkettedik() {
+            string[] input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+            int K = int.Parse(input[2]);
+
+            for (int x = 0; x <= K / N; x++) {
+                int remainingHeads = K - N * x;
+
+                if (remainingHeads >= 0 && remainingHeads % M == 0) {
+                    int y = remainingHeads / M;
+                    Console.WriteLine($"{x} {y}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("0 0");
+        }
+
+        static void tizenharmadik() {
+            long n = long.Parse(Console.ReadLine());
+
+            if (n < 0) {
+                Console.WriteLine(0);
+                return;
+            }
+
+            if (n == 0) {
+                Console.WriteLine(1);
+                return;
+            }
+
+            long a = 0; // F0
+            long b = 1; // F1
+            int index = 1; // F1 i
+
+            while (b <= n) {
+                long next = a + b;
+                a = b;
+                b = next;
+                index++;
+            }
+
+            Console.WriteLine(index);
+        }
+
+        static void tizennegyedik() {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 0) {
+                return;
+            }
+
+            // növekvő
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine(new string('*', i));
+            }
+
+            Console.WriteLine();
+
+            // 2. növekvő, eltolva
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine(new string(' ', n - i) + new string('*', i));
+            }
+
+            Console.WriteLine();
+
+            // csökkenő, eltolva
+            for (int i = n; i >= 1; i--) {
+                Console.WriteLine(new string(' ', n - i) + new string('*', i));
+            }
+
+            Console.WriteLine();
+
+            // csökkenő
+            for (int i = n; i >= 1; i--) {
+                Console.WriteLine(new string('*', i));
+            }
+
+            Console.WriteLine();
+
+            // Piramis: 2N hosszúságú
+            for (int i = 1; i <= 2 * n; i += 2) { // 1-től indul és 2-vel nő, és addig megy amig i != 2 * n (a legnagyobb sorban 2 * n - 1 * lesz!)
+                int spaces = (2 * n - i) / 2; // hány szóköz kell, piramis alapja -> (2 * n - 1) / 2
+                Console.WriteLine(new string(' ', spaces) + new string('*', i) + new string(' ', spaces));
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,30 @@
+# Optimalizáció Dokumentum
+
+## Táblázat magyarázat
+
+- Runtime: Futási idő, hogy az adott metódus mennyi idő alatt futott le.
+- Memory Allocation: A memóriaterület lefoglalása az adatok számára.
+- Gen0: A rövid életű objektumok gyors memória-felszabadítási folyamata. (Garbage Collection)
+
+✅❌
+
+# Benchmark Táblázat & Eredmények
+
+Amelyik nincsen ott az elhanyagolhatónak tekinthető. 
+
+| Feladat | Megközelítés | Runtime | Memory Allocation | Gen0 | Kép |
+|---------|-------------|-------------|------------------|------------------|---|
+| 1. feladat | LINQ | 9,45 μs | 12,93 KB | 8,40 | ![](https://i.imgur.com/8el0Sgg.png)
+| | StringBuilder ✅ | 7,32 μs | 7,66 KB | 4,98 |
+| 2. feladat | LINQ | 2,19 μs | 1214 B | 0,77 | ![](https://i.imgur.com/z4liOmx.png)
+| | For loop ✅ | 1,55 μs | 437 B | 0,27 |
+| 3. feladat | LINQ ✅ | 506 ns | 244 B | 0,15 | ![](https://i.imgur.com/At2Lquq.png)
+| 4. feladat (982451653) | LINQ  | 129,3 μs | 84 B | - | ![](https://i.imgur.com/93JU2Dq.png)
+| | For loop ✅ | 17,19 μs | - | - |
+| 5. feladat | Hagyományos ✅ | 504 ns | 200 B | 0,12 | Elfelejtettem képet csinálni
+| 6. feladat | LINQ  | 155,9 ns | 256 B | 0,162 | ![](https://i.imgur.com/v8Tx3LP.png)
+| | For loop ✅ | 2,09 ns | - | - |
+| 9. feladat (615154) | For loop (gyökvonás) ✅ | 859,2 ns | - | - | ![](https://i.imgur.com/Jr84MMM.png)
+| 10. feladat | For loop (Euklidészi) ✅ | 4,6 ns | - | - | ![](https://i.imgur.com/xctvPZD.png)
+| | LINQ  | 68,63 ns | 120 B | 0,07 |
+| 11. feladat | For loop ✅ | 154,8 ns | 136 B | 0,08 | ![](https://i.imgur.com/hWGQBaF.png)
